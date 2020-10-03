2e39f8e34384878068dd15d6292f1c3ead17c097
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-10-03 00:19:41 -0600

    CharacterMoverScript

---
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterMover : MonoBehaviour
+{
+    public Rigidbody rBody;
+    private float moveSpeed;
+    public float walkSpeed = 10;
+    public float runSpeed = 35;
+    private float xMove, zMove;
+    
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        xMove = Input.GetAxis("Horizontal");
+        zMove = Input.GetAxis("Vertical");
+        
+        if (Input.GetKey(KeyCode.LeftShift))
+            moveSpeed = runSpeed;
+        else
+            moveSpeed = walkSpeed;
+        
+        Vector3 movement = new Vector3(xMove, 0, zMove) * (moveSpeed * Time.deltaTime);
+        rBody.MovePosition((transform.position + movement));
+    }
+}
