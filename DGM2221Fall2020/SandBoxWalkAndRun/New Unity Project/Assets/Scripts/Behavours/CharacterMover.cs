1f4882325bcd37911909d2d4dcba88ee4ac33d9a
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-10-12 22:49:47 -0600

    All scriptable objects and walk_Run with Jump

--- DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/CharacterMover.cs
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/CharacterMover.cs
@@ -14,6 +14,6 @@ public class CharacterMover : MonoBehaviour
     
     void Update()
     {
-        movePattern.Movement(controller, transform);
+        movePattern.Move(controller, transform);
     }
 }
--- DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/ScriptableObjects/MoveData.cs
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/ScriptableObjects/MoveData.cs
@@ -1,48 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu]
-public class MoveData : ScriptableObject
-{
-    public FloatData walkSpeed, runSpeed, jumpForce, gravity, rotateSpeed;
-    public IntData jumpCount, jumpCountMax;
-    public BoolData canJump;
-    public float yVar;
-    private Vector3 moveDirection;
+﻿using UnityEngine;
 
-    public float SetSpeed()
+    [CreateAssetMenu]
+    public class MoveData : ScriptableObject
     {
-        if (Input.GetKey(KeyCode.LeftShift))
+        public FloatData walkSpeed, runSpeed;
+        public FloatData jumpForce;
+        public FloatData gravity, rotateSpeed;
+        public IntData jumpCount, jumpCountMax;
+        public BoolData canJump;
+        public float yVar;
+        private Vector3 moveDirection;
+
+        
+        public void Move(CharacterController controller, Transform transform)
         {
-            return runSpeed.value;
+            var vInput = Input.GetAxis("Vertical") * SetSpeed();
+            moveDirection.Set(vInput,yVar,0);
+
+            var hInput = Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed.value;
+            transform.Rotate(0, hInput, 0);
+
+            yVar -= gravity.value * Time.deltaTime;
+
+            if (controller.isGrounded && moveDirection.y < 0)
+            {
+                jumpCount.value = 0;
+            }
+
+            if (Input.GetButtonDown("Jump") && jumpCount.value <= jumpCountMax.value && canJump.value)
+            {
+                yVar = jumpForce.value;
+                jumpCount.value++;
+                Debug.Log("working");
+            }
+
+            moveDirection = transform.TransformDirection(moveDirection);
+            controller.Move(moveDirection * Time.deltaTime);
         }
-        else
-        {
-            return walkSpeed.value;
-        }
+        public float SetSpeed()
+                      {
+                          if (Input.GetKey(KeyCode.LeftShift))
+                          {
+                              return runSpeed.value;
+                          }
+                          else
+                          {
+                              return walkSpeed.value;
+                          }
+                      }
     }
-    public void Movement(CharacterController controller, Transform transform)
-    {
-        var vInput = Input.GetAxis("Vertical") * SetSpeed();
-        moveDirection.Set(vInput,yVar,0);
-
-        var hInput = Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed.value;
-        transform.Rotate(0, hInput, 0);
-
-        yVar -= gravity.value * Time.deltaTime;
-
-        if (controller.isGrounded && moveDirection.y < 0)
-        {
-            jumpCount.value = 0;
-        }
-
-        if (Input.GetButtonDown("Jump") && jumpCount.value <= jumpCountMax.value && canJump.value)
-        {
-            yVar = jumpForce.value;
-            jumpCount.value++;
-        }
-
-        moveDirection = transform.TransformDirection(moveDirection);
-        controller.Move(moveDirection * Time.deltaTime);
-    }
-}
