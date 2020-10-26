28da39b24c0802da082832c535ef214f28c69a01
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-10-25 21:09:16 -0600

    shooting

---
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/AddForceBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class AddForceBehaviour : MonoBehaviour
+{
+    private Rigidbody rBody;
+
+    public FloatData force;
+
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        var forceDirection = new Vector3(force.value, 0,0);
+        rBody.AddRelativeForce(forceDirection);
+    }
+}
---
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/InstancerBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class InstancerBehaviour : MonoBehaviour
+{
+    public GameObject prefab;
+    public Vector3Data rotationDirection;
+
+    public void Instance()
+    {
+        var location = transform.position;
+        var newObj = Instantiate(prefab, location, Quaternion.Euler(this.rotationDirection.value));
+    }
+}
