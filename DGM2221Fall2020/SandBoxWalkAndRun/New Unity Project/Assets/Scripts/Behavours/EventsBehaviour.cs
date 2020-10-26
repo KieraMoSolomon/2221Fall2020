7f9ce5692fe08146195038fe58881eacb9aff3d9
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-10-25 22:53:58 -0600

    TriggerBoxesDone

--- DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/EventsBehaviour.cs
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/EventsBehaviour.cs
@@ -7,9 +7,21 @@ using UnityEngine.Events;
 public class EventsBehaviour : MonoBehaviour
 {
     public UnityEvent mouseDownEvent;
-
+    public UnityEvent onTriggerEvent;
+    public UnityEvent exitTriggerEvent;
+    
     public void OnMouseDown()
     {
         mouseDownEvent.Invoke();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        onTriggerEvent.Invoke();
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        exitTriggerEvent.Invoke();
     }
 }
---
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/HeightCheckBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightCheckBehaviour : MonoBehaviour
+{
+    public FloatData height;
+    
+    public void HeightCheck()
+    {
+        if (height.value >= 2)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+        //if too small change to red
+    }
+
+    public void ReturnNormal()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.grey;
+    }
+}
