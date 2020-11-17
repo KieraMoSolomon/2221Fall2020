c948da3d959574ffcc5ebb1efdc85823e0d5146f
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-11-16 22:39:03 -0700

    better ground

---
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/CameraBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    
+    public Vector3Data aimPoint, offsetCam;
+    [Range(0.01f, 1.0f)]
+    public float smoothFactor = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offsetCam.value = transform.position - aimPoint.value;
+        Vector3 newPos = aimPoint.value + offsetCam.value;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPos = aimPoint.value + offsetCam.value;
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+    }
+}
---
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/CharacterBehaviour.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterBehaviour : MonoBehaviour
+{
+    public Vector3Data currentPosition;
+    public float rotateSpeed = 120f, gravity = -9.81f, jumpForce = 10f;
+    public FloatData normalSpeed, fastSpeed;
+    public IntData playerJumpCount;
+    
+    protected CharacterController controller;
+    protected Vector3 movement;
+    protected bool canMove = true;
+    protected readonly WaitForFixedUpdate wffu = new WaitForFixedUpdate();
+    protected float vInput, hInput;
+    protected FloatData moveSpeed;
+    
+    protected float yVar;
+    private int jumpCount;
+
+    private void OnEnable()
+    {
+        moveSpeed = normalSpeed;
+        controller = GetComponent<CharacterController>();
+        StartCoroutine(Move());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    protected IEnumerator Move()
+    {
+        canMove = true;
+        while (canMove)
+        {
+            OnMove();
+            yield return wffu;
+        }
+    }
+    
+    protected virtual void OnHorizontal()
+    {
+        hInput = Input.GetAxis("Horizontal")*Time.deltaTime*rotateSpeed;
+        transform.Rotate(0,hInput,0);
+    }
+
+    protected virtual void OnVertical()
+    {
+        vInput = Input.GetAxis("Vertical")*moveSpeed.value;
+        movement.Set(0,yVar,vInput);
+    }
+
+    private void OnMove()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            moveSpeed = fastSpeed;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            moveSpeed = normalSpeed;
+        }
+        
+        OnVertical();
+        OnHorizontal();
+
+        yVar += gravity*Time.deltaTime;
+
+        if (controller.isGrounded && movement.y < 0)
+        {
+            yVar = -1f;
+            jumpCount = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && jumpCount < playerJumpCount.value)
+        {
+            yVar = jumpForce;
+            jumpCount++;
+        }
+
+        currentPosition.value = transform.position;
+        movement = transform.TransformDirection(movement);
+        controller.Move((movement) * Time.deltaTime);
+    }
+}
