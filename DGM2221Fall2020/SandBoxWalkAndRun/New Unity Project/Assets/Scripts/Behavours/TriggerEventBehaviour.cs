73c90d632f5592ab08da666e7986d001e4d70e99
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-11-18 22:42:42 -0700

    Working on ride movements

---
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/TriggerEventBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventBehaviour : MonoBehaviour
+{
+    public UnityEvent triggerEnterEvent, triggerExitEvent;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        triggerExitEvent.Invoke();
+    }
+}
