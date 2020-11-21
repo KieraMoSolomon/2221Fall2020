a198d7fd10a6ce09c6764fe82c04a75430d3d876
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-11-20 23:27:49 -0700

    Working on Flying ride codes

---
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/FlyingRide/BlockFlyingBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockFlyingBehaviour : MonoBehaviour
+{
+    public bool canUp, canDown;
+    public float minHeight;
+    
+    private Vector3 position, rotation;
+    public float moveSpeed = 3f, maxHeight = 5f;
+    private CharacterController controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (canDown && Input.GetKey(KeyCode.DownArrow))
+        {
+            position.y = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+            canUp = true;
+            
+            if (position.y <= minHeight)
+            {
+                canDown = false;
+            }
+        }
+        
+        var limit = transform.position;
+        
+        position.y = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        
+        controller.Move(position);
+    }
+    
+}
