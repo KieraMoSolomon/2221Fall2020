aa18acd80acd5b333e9118159e6f8eaf849640d6
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-11-27 13:32:40 -0700

    Got it to change scenes when done with 2 rotations

---
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/SceneManagementBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagementBehaviour : MonoBehaviour
+{
+    public int sceneIndex;
+
+    public void OnTriggerEnter()
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
