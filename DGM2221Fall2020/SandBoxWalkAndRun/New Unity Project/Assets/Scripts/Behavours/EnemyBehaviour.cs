d3d3a21dc98f3356e5fc8d80995eead671bce860
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-12-01 14:59:24 -0700

    Gargoyles can be destroyed and update health for them

---
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/EnemyBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public float individualHealth;
+    public FloatData health;
+    public void Start()
+    {
+        individualHealth = health.value;
+    }
+
+    public void UpdateHealth()
+    {
+        Debug.Log("update gargoyle health");
+        individualHealth -= 1;
+        if (individualHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
