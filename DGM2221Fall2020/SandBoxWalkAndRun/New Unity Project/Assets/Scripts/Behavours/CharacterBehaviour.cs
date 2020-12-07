12157521446610cac438d9823a312604b9faf0ec
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-12-07 06:49:25 -0700

    Adding Unity Actions

--- DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/CharacterBehaviour.cs
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/CharacterBehaviour.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 [RequireComponent(typeof(CharacterController))]
 public class CharacterBehaviour : MonoBehaviour
