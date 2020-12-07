70e4cfb1e2efb44123e8ffb3b73a4cfc345b3223
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-12-06 22:33:52 -0700

    saving before breaking with AI

---
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Enemies/AIBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIBehaviour : MonoBehaviour
+{
+    private bool canHunt, canPatrol;
+    private NavMeshAgent agent;
+    private WaitForFixedUpdate wffu;
+    private WaitForSeconds wfs;
+
+    public float holdTime = 2f;
+    public Transform player;
+    
+    public List<Transform> patrolPoints;
+    private int i = 0;
+    private void Start()
+    {
+        wfs = new WaitForSeconds(holdTime);
+        wffu = new WaitForFixedUpdate();
+        agent = GetComponent<NavMeshAgent>();
+        StartCoroutine(Patrol());
+    }
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        canHunt = true;
+        canPatrol = false;
+        agent.destination = player.position;
+        var distance = agent.remainingDistance;
+        while (distance <= 0.25f)
+        {
+            distance = agent.remainingDistance;
+            yield return wffu;
+        }
+        yield return wfs;
+        StartCoroutine(canHunt ? OnTriggerEnter(other) : Patrol());
+    }
+
+    private IEnumerator Patrol()
+    {
+        canPatrol = true;
+        while (canPatrol)
+        {
+            yield return wffu;
+            if (agent.pathPending || !(agent.remainingDistance < 0.5f)) continue;
+            agent.destination = patrolPoints[i].position;
+            i = (i + 1) % patrolPoints.Count;
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        canHunt = false;
+    }
+}
--- DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/EnemyBehaviour.cs
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/EnemyBehaviour.cs
@@ -7,8 +7,13 @@ public class EnemyBehaviour : MonoBehaviour
 {
     public float individualHealth;
     public FloatData health;
+    public FloatData speed;
+    public bool canDash;
+    public WaitForSeconds wfs;
+    public float holdTime = 2f;
     public void Start()
     {
+        wfs = new WaitForSeconds(holdTime);
         individualHealth = health.value;
     }
 
@@ -20,5 +25,28 @@ public class EnemyBehaviour : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    public void DragonDash()
+    {
+        //on trigger stay
+        //find player position
+        if (canDash)
+        {
+            MoveDragon();
+        }
+    }
+
+    private void MoveDragon()
+    {
+        //have head go after player then go back to normal gameObject.transform.position;
+        StartCoroutine(HoldDash());
+    }
+
+    private IEnumerator HoldDash()
+    {
+        canDash = false;
+        yield return wfs;
+        canDash = true;
     }
 }
