138630ee3bd91950c9396dcbea08655e4dfeefa6
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-12-12 22:07:24 -0700

    Got shooting to ignore trigger boxes

--- DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/FollowMouseBehaviour.cs
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/FollowMouseBehaviour.cs
@@ -14,7 +14,7 @@ public class FollowMouseBehaviour : MonoBehaviour
 
     public void Update()
     {
-        if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out var hit, 5000))
+        if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out var hit, 100))
         {
             postionOfMouse.value = hit.point;
         }
