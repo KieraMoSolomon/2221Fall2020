db420ce4e25fac08b7fcdf258b53848499ab16df
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-12-15 12:14:53 -0700

    working on start menu

---
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/ScriptableObjects/LevelLoaderSO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu]
+public class LevelLoaderSO : ScriptableObject
+{
+    public void Load(string level)
+    {
+        SceneManager.LoadScene(level, LoadSceneMode.Additive);
+    }
+}
