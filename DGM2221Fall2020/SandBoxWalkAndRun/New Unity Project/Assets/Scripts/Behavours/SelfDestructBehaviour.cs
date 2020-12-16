27465fec99170dc6cf3d8f4271cde7f22c5a782f
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-12-16 02:08:01 -0700

    Got the hud Working

--- DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/SelfDestructBehaviour.cs
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/SelfDestructBehaviour.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 
 public class SelfDestructBehaviour : MonoBehaviour
 {
-    public float holdTime = 2f;
-    IEnumerator Start()
-    {
-        yield return new WaitForSeconds(holdTime);
-        Destroy(gameObject);
-    }
+    // public float holdTime = 2f;
+    // IEnumerator Start()
+    // {
+    //     yield return new WaitForSeconds(holdTime);
+    //     Destroy(gameObject);
+    // }
 
     public void TriggerSelfDestruct()
     {
