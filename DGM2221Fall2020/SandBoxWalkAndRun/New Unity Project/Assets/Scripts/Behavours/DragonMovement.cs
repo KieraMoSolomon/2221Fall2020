ff76f763b792b3a1b3e1a0d8412634af1a3c797f
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-12-16 03:20:37 -0700

    Save before I crash unity with while loops in a coroutine

---
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/DragonMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+//[RequireComponent(typeof(CharacterController))]
+public class DragonMovement : MonoBehaviour
+{
+    public Rigidbody rBody;
+    //public CharacterController controller;
+    public Vector3Data playerPos;
+    public Vector3 defaultPos, dragonToPlayer, targetPosition;
+    public bool canDash;
+    private WaitForSeconds wfs;
+    public float holdTime = 2f;
+    public float speed = 10f;
+    public void Start()
+    {
+        wfs = new WaitForSeconds(holdTime);
+        rBody = GetComponent<Rigidbody>();
+        //controller = GetComponent<CharacterController>();
+        defaultPos = transform.position;
+    }
+
+    public void DragonDash()
+    {
+        if (canDash)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        //controller.Move(playerPos.value * (speed * Time.deltaTime));
+        while (transform.position != playerPos.value)
+        {
+            rBody.velocity = (playerPos.value - defaultPos).normalized *speed;
+        }
+         yield return wfs;
+         while (transform.position != defaultPos)
+         {
+             rBody.velocity = (defaultPos - transform.position).normalized * speed;
+         }
+
+         canDash = true;
+        //controller.Move(defaultPos * (speed * Time.deltaTime));
+        //rBody.MovePosition(defaultPos *(speed *Time.deltaTime));
+        Debug.Log("Please Work");
+    }
+}
