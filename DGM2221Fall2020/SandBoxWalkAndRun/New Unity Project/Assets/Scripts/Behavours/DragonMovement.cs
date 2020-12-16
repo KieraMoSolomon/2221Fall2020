af5b865e38c722bdd8b4551a6610aa7ca623cebb
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-12-16 05:53:09 -0700

    try 17

--- DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/DragonMovement.cs
+++ DGM2221Fall2020/SandBoxWalkAndRun/New Unity Project/Assets/Scripts/Behavours/DragonMovement.cs
@@ -26,27 +26,33 @@ public class DragonMovement : MonoBehaviour
 
     public void Update()
     {
-        if (canDash)
+        if (transform.position != targetPosition && canDash)
         {
-            targetPosition = playerPos.value;
-            distFloat = Vector3.Distance(defaultPos, targetPosition);
-            if (distFloat <= 2f)
-            {
-                transform.position += defaultPos * (speed * Time.deltaTime);
-            }
-            else
-            {
-                transform.position += targetPosition * (speed * Time.deltaTime);
-            }
+            transform.position += targetPosition * (speed * Time.deltaTime);
+            Debug.Log(targetPosition);
+        }
+
+        if (transform.position != defaultPos && canDash == false)
+        {
+            transform.position += defaultPos * (Time.deltaTime * speed);
         }
     }
 
     public void DragonDash()
     {
+        targetPosition = playerPos.value;
         StartCoroutine(Dash());
     }
 
     private IEnumerator Dash()
+    {
+        canDash = true;
+        yield return wfs;
+        canDash = false;
+        StartCoroutine(DashHold());
+    }
+
+    private IEnumerator DashHold()
     {
         canDash = false;
         yield return wfs;
